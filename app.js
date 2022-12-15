0a7f8c2aba2290cc42fdb06b22a9e1e98c4808b1
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2022-12-15 15:34:03 +0800

    update: complete tail method

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ class Node {
 
 class LinkedList {
     constructor() {
-        this.beginning = null;
+        this.Head = null;
         this.length = 0;
     }
 
     // 1. Insert new node to the start of the list.
     append(data) {
-        this.beginning = new Node(data, this.beginning);
+        this.Head = new Node(data, this.Head);
         this.length++;
     }
     // 2. Insert new node to the end of the list.
@@ -21,11 +21,11 @@ class LinkedList {
         let node = new Node(data);
         let current;
 
-        // If empty, make beginning
-        if(!this.beginning) {
-            this.beginning = node;
+        // If empty, make head
+        if(!this.Head) {
+            this.Head = node;
         } else {
-            current = this.beginning;
+            current = this.Head;
 
             while (current.next) {
                 current = current.next
@@ -40,26 +40,35 @@ class LinkedList {
         console.log(this.length);
     }
 
-    // 4. Prints the first node in the list
+    // 4. Prints the first node in the list.
     head() {
-        console.log(this.beginning);
+        console.log(this.Head);
     }
-    // Print list data
-    // printListData() {
-    //     let current = this.beginning;
 
-    //     while (current) {
-    //         console.log(current.data);
-    //         current = current.next;
-    //     }
-    // }
+    // 5. Prints the last node in the list.
+    tail() {
+        let current = this.Head;
+        while (current) {
+            if (!current.next) console.log(current);
+            current = current.next;
+        }
+    }
+    // Print every node in the list.
+    printListData() {
+        let current = this.Head;
+
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
 }
 
 
 const ll = new LinkedList();
 
 ll.append(200);
-ll.append(500);
-ll.prepend(400);
+ll.append(400);
+ll.prepend(500);
 
 ll.head();
